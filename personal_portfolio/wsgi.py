42f7f60c792fa2b1cc716c80f51f24b9340ea34d
kamal <skamalkumar@gmail.com>
2022-08-12 23:50:21 +0530

    first commit

--- personal_portfolio/wsgi.py
+++ personal_portfolio/wsgi.py
@@ -15,4 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_portfolio.settings')
 
 application = get_wsgi_application()
 
-application = DjangoWhiteNoise(application)
+"application = DjangoWhiteNoise(application)"
