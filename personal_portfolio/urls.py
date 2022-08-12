42b93afd96be3e189876fa42c7c7f4fad248ad3d
kamal <skamalkumar@gmail.com>
2022-08-12 22:58:52 +0530

    first commit

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -19,16 +19,14 @@ from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views
 from django.views.static import serve
-# from django.conf.urls import url
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('blog/', include('blog.urls')),
-    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
