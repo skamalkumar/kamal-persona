1ee82b9c1f8bf94e827853eecc9be7a9de4b5652
kamal <skamalkumar@gmail.com>
2022-08-13 00:12:08 +0530

    first commit

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -126,9 +126,6 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/images/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
 # MEDIA_ROOT = BASE_DIR / '/media/images/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
