e3107520b1197d4f293e543a15ebb05a6ff7e3be
kamal <skamalkumar@gmail.com>
2022-08-12 23:43:26 +0530

    first commit

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -138,3 +138,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # except ImportError:
 #     print("Looks like no local file. You must be on production")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
