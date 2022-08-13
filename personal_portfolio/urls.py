e2c9532fc0a1b09f80d7069a2da167e6f3038d92
kamal <skamalkumar@gmail.com>
2022-08-13 16:42:13 +0530

    First commit

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -24,8 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('blog/', include('blog.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-  urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
